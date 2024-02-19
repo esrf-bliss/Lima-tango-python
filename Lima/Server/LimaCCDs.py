64f7a11c9de675eb01c0e3a6aed506a164898a3f
Alejandro Homs Puron <ahoms@esrf.fr>
2024-02-19 16:55:29 +0100

    Add BufferHelper API for accumulation

--- Lima/Server/LimaCCDs.py
+++ Lima/Server/LimaCCDs.py
@@ -658,6 +658,7 @@ class LimaCCDs(PyTango.LatestDeviceImpl):
                 },
             }
             self.__BufferParamData = {
+                "acc": {"attr_split": ["buffer"], "name": "Buffer"},
                 "saving": {"attr_split": ["zbuffer"], "name": "ZBuffer"},
             }
 
@@ -2378,6 +2379,12 @@ class LimaCCDsClass(PyTango.DeviceClass):
             "Number of thread for processing",
             [2],
         ],
+        "AccBufferParameters": [
+            PyTango.DevString,
+            "Accumulation Buffer alloc. params: "
+            "<initMem=0|1, durationPolicy=EPHEMERAL|PERSISTENT, sizePolicy=AUTOMATIC|FIXED, reqMemSizePercent=0-100>",
+            [''],
+        ],
         "AccThresholdCallbackModule": [
             PyTango.DevString,
             "Plugin name file which manage threshold",
@@ -2547,6 +2554,18 @@ class LimaCCDsClass(PyTango.DeviceClass):
             [PyTango.DevLong, PyTango.SCALAR, PyTango.READ_WRITE]
         ],
         "acc_saturated_cblevel": [
+            [PyTango.DevLong, PyTango.SCALAR, PyTango.READ_WRITE]
+        ],
+        "acc_buffer_init_mem": [
+            [PyTango.DevBoolean, PyTango.SCALAR, PyTango.READ_WRITE]
+        ],
+        "acc_buffer_duration_policy": [
+            [PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]
+        ],
+        "acc_buffer_size_policy": [
+            [PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]
+        ],
+        "acc_buffer_req_mem_size_percent": [
             [PyTango.DevLong, PyTango.SCALAR, PyTango.READ_WRITE]
         ],
         "acq_mode": [[PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]],
